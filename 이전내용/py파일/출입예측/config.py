02683221632d1ab303abd1ce829873d054ff1f9f
금현준 <qhofr3567@gmail.com>
2023-01-17 22:56:24 +0900

    final

--- 이전내용/py파일/출입예측/config.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-host = config["impala"]["host"]
-port = int(config["impala"]["port"])
-user = config["impala"]["user"]
-password = config["impala"]["password"]
